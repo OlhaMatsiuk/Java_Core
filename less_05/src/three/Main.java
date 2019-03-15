e7582da2198053bc3724ace619e5a02cacbbd91f
Olha Zakharchuk <o.zakharchuk13@gmail.com>
2019-03-15 20:57:16 +0200

    f

--- less_05/src/three/Main.java
+++
@@ -1,9 +0,0 @@
-package three;
-
-public class Main {
-	public static void main(String[] args) {
-
-		System.out.println("Float=  " + Float.MAX_VALUE);
-		System.out.println("Double=  " + Double.MAX_VALUE);
-		}
-}
